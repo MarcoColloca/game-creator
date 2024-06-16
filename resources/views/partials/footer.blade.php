32ecdaeffd8ab327e8d2a780c566a7c2303c7a58
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-17 00:05:46 +0200

    Add→ changed color from green to coral

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col py-3">
-                    <h1 class="text-success text-center">
+                    <h1 class="text-coral text-center">
                         I am a Wonderful Footer!
                     </h1>                       
                 </div>               

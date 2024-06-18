572e378d8c0cff13bd17104dec7d491ba8f5d4b2
Francesca Barletta <wallebar@gmail.com>
2024-06-18 16:46:44 +0200

    ADD:style in create and edit of types

--- resources/views/admin/types/create.blade.php
+++ resources/views/admin/types/create.blade.php
@@ -3,28 +3,29 @@
 @section('title', 'Type Creation')
 
 @section('content')
+<div class="char-bg">
 <section class="mt-5 py-1">
-    <div class="container bg-dark py-4">
-        <h1 class="title text-center text-success">Add a new Type!</h1>
+    <div class="container title-container p-2 mb-3 rounded-3 shadow-jg">
+        <h1 class="title text-center">Add a new Type!</h1>
     </div>
 </section>
 
 
 <section class="mb-5 py-1">
-    <div class="bg-light container py-4">
+    <div class="container rounded-3 char-card py-4">
         <form action="{{ route('admin.types.store') }}" method="POST">
             @csrf
             <div class="mb-3">
-                <label for="name" class="form-label fw-bold">Name</label>
+                <label for="name" class="form-label fw-bold text-coral">Name</label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="Insert mew Type's name"
                     value="{{ old('name') }}">
             </div>
             <div class="mb-3">
-                <label for="description" class="form-label fw-bold">Description</label>
+                <label for="description" class="form-label fw-bold text-coral">Description</label>
                 <textarea class="form-control" id="description" name="description"
                     placeholder="Describe your type">{{ old('description') }}</textarea>
             </div>
-            <button class="btn btn-primary">Add</button>
+            <button class="btn btn-orange">Add</button>
         </form>
     </div>
 
@@ -40,4 +41,5 @@
         @endif
     </div>
 </section>
+</div>
 @endsection
--- resources/views/admin/types/edit.blade.php
+++ resources/views/admin/types/edit.blade.php
@@ -3,30 +3,31 @@
 @section('title', 'Edit Type')
 
 @section('content')
+<div class="char-bg">
 <section class="mt-5 py-1">
-    <div class="container bg-dark py-4">
-        <h1 class="title text-center text-success">Edit your type!</h1>
+    <div class="container title-container p-2 mb-3 rounded-3 shadow-jg">
+        <h1 class="title text-center">Edit your type!</h1>
     </div>
 </section>
 
 
 <section class="mb-5 py-1">
-    <div class="bg-light container py-4">
+    <div class="container rounded-3 char-card py-4">
         <form action="{{ route('admin.types.update', $type) }}" method="POST">
             @csrf
             @method('PUT')
             <div class="mb-3">
-                <label for="name" class="form-label fw-bold">Name</label>
+                <label for="name" class="form-label fw-bold text-coral">Name</label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="Inserisci il nome"
                     value="{{ old('name', $type->name) }}">
             </div>
 
             <div class="mb-3">
-                <label for="description" class="form-label fw-bold">Description</label>
+                <label for="description" class="form-label fw-bold text-coral">Description</label>
                 <textarea class="form-control" id="description" name="description"
                     placeholder="Inserisci la descrizione">{{ old('description', $type->description) }}</textarea>
             </div>
-            <button class="btn btn-primary">Save</button>
+            <button class="btn btn-orange">Save</button>
         </form>
     </div>
 
@@ -42,4 +43,5 @@
         @endif
     </div>
 </section>
+</div>
 @endsection

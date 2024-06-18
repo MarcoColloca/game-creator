ffd6742a727b171c3cedf743cf0686b6be90a55d
Francesca Barletta <wallebar@gmail.com>
2024-06-18 16:43:07 +0200

    ADD: style in edit and create of Items

--- resources/views/admin/items/edit.blade.php
+++ resources/views/admin/items/edit.blade.php
@@ -3,33 +3,34 @@
 @section('title', 'Character')
 
 @section('content')
+<div class="char-bg">
 <section class="mt-5 py-1">
-    <div class="container bg-dark py-4">
-        <h1 class="title text-center text-warning">Edit your Item!</h1>
+    <div class="container title-container p-2 mb-3 rounded-3 shadow-jg">
+        <h1 class="title text-center">Edit your Item!</h1>
     </div>
 </section>
 
 
 <section class="mb-5 py-1">
-    <div class="bg-light container py-4">
+    <div class="container rounded-3 char-card py-4">
         <form action="{{ route('admin.items.update', $item) }}" method="POST">
             @csrf
             @method('PUT')
 
             <div class="mb-3">
-                <label for="name" class="form-label fw-bold">Name</label>
+                <label for="name" class="form-label fw-bold text-coral">Name</label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="Insert your item's name"
                     value="{{ old('name', $item->name) }}">
             </div>
 
             <div class="mb-3">
-                <label for="slug" class="form-label fw-bold">slug</label>
+                <label for="slug" class="form-label fw-bold text-coral">slug</label>
                 <input type="text" class="form-control" id="slug" name="slug" placeholder="nome-item"
                     value="{{ old('slug', $item->slug) }}">
             </div>
             
 
-            <label for="category" class="form-label fw-bold">Category</label>
+            <label for="category" class="form-label fw-bold text-coral">Category</label>
             <select class="form-select mb-3" aria-label="Default select example" name="category" id="category">
                 <option value="Simple Melee Weapons">Simple Melee Weapon</option>
                 <option value="Simple Ranged Weapons">Simple Ranged Weapon</option>
@@ -38,25 +39,25 @@
             </select>
             
             <div class="mb-3">
-                <label for="weight" class="form-label fw-bold">Weight (lb)</label>
+                <label for="weight" class="form-label fw-bold text-coral">Weight (lb)</label>
                 <input type="number" class="form-control" id="weight" name="weight" placeholder="Insert value es. 15"
                     value="{{ old('weight', $item->weight) }}">
             </div>
 
             <div class="mb-3">
-                <label for="cost" class="form-label fw-bold">Cost</label>
+                <label for="cost" class="form-label fw-bold text-coral">Cost</label>
                 <input type="number" class="form-control" id="cost" name="cost" placeholder="Insert value es. 21"
                     value="{{ old('cost', $item->cost) }}">
             </div>
 
-            <label for="coin"  class="form-label fw-bold">Coin</label>
+            <label for="coin"  class="form-label fw-bold text-coral">Coin</label>
             <select class="form-select mb-3" aria-label="Default select example" name="coin" id="coin">
                 <option value="cp">cp</option>
                 <option value="sp">sp</option>
                 <option selected value="gp">gp</option>
             </select>
             
-            <label for="damage_dice"  class="form-label fw-bold">Damage Dice </label>
+            <label for="damage_dice"  class="form-label fw-bold text-coral">Damage Dice </label>
             <select class="form-select mb-3" aria-label="Default select example" name="damage_dice" id="damage_dice">
                 <option value="0">0</option> 
                 <option value="1">1</option>     
@@ -70,13 +71,13 @@
 
 
             <div class="mb-3">
-                <label for="description" class="form-label fw-bold">Description</label>
+                <label for="description" class="form-label fw-bold text-coral">Description</label>
                 <textarea class="form-control" id="description" name="description" placeholder="Describe your item">
                     {{ old('description', $item->description) }}
                 </textarea>
             </div>
 
-            <button class="btn btn-primary">Edit</button>
+            <button class="btn btn-orange">Edit</button>
         </form>
     </div>
 
@@ -92,4 +93,5 @@
         @endif
     </div>
 </section>
+</div>
 @endsection
